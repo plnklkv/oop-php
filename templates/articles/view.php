846ba55c1558ea584dd1609aa37426f3b6f42324
plnklkv <polinakulakova04072@gmail.com>
2022-11-17 13:55:58 +0700

    Делаем вывод статей на сайте из базы данных

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,7 +1,5 @@
 <?php include __DIR__ . '/../header.php'; ?>
-<?php foreach ($articles as $article): ?>
-    <h2><a href="/articles/<?= $article['id'] ?>"><?= $article['name'] ?></a></h2>
+    <h1><?= $article['name'] ?></h1>
     <p><?= $article['text'] ?></p>
     <p>Имя автора: <?= $nickname ?></p>
-<?php endforeach; ?>
 <?php include __DIR__ . '/../footer.php'; ?>
