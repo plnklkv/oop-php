a37aed6f76ac2c307736a8059d4967c8e51b5f7a
plnklkv <polinakulakova04072@gmail.com>
2022-11-17 13:06:47 +0700

    Класс для работы с базой данных

--- www/index.php
+++ www/index.php
@@ -4,23 +4,6 @@ spl_autoload_register(function (string $className) {
     require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
-/*
-echo '<br>1 Неймспейсы и автозагрузка в PHP <br>';
-$author = new \MyProject\Models\Users\User('Иван');
-$article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
-var_dump($article);
-
-
-echo '<br><br>2 Controller в MVC <br>';
-$controller = new \MyProject\Controllers\MainController();
-
-if (!empty($_GET['name'])) {
-    $controller->sayHello($_GET['name']);
-} else {
-    $controller->main();
-}
-
-echo '<br><br>3 Фронт-контроллер и роутинг в PHP <br>';*/
 $route = $_GET['route'] ?? '';
 $routes = require __DIR__ . '/../src/routes.php';
 
