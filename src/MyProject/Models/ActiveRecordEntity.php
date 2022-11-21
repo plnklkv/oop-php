04ec7d05cc705bb778b72140a828317927e95972
plnklkv <polinakulakova04072@gmail.com>
2022-11-21 10:56:20 +0700

    Паттерн Singleton в PHP

--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -26,13 +26,13 @@ abstract class ActiveRecordEntity
 
     public static function findAll(): array
     {
-        $db = new Db();
+        $db = Db::getInstance();
         return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
     }
 
     public static function getById(int $id): ?self
     {
-        $db = new Db();
+        $db = Db::getInstance();
         $entities = $db->query(
             'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
             [':id' => $id],
