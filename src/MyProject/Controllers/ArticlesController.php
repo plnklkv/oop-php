74e6c91d38232890115a5a53de05f50ccdd33ac3
plnklkv <polinakulakova04072@gmail.com>
2022-11-21 15:07:19 +0700

    Обновление с помощью Active Record

--- src/MyProject/Controllers/ArticlesController.php
+++ src/MyProject/Controllers/ArticlesController.php
@@ -10,6 +10,10 @@ class ArticlesController
     /** @var View */
     private $view;
 
+    protected $name;
+
+    protected $text;
+
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
@@ -27,5 +31,31 @@ class ArticlesController
         $this->view->renderHtml('articles/view.php', [
             'article' => $article
         ]);
+    }
+
+    public function edit(int $articleId): void
+    {
+        $article = Article::getById($articleId);
+
+        if ($article === null) {
+            $this->view->renderHtml('errors/404.php', [], 404);
+            return;
+        }
+
+        $article->setName('Новое название статьи');
+        $article->setText('Новый текст статьи');
+
+        var_dump($article);
+    }
+
+    public function add():void
+    {
+        $article = new Article();
+        $article->setName('Новая статья 3');
+        $article->setText('Новый текст 3');
+        $article->setAuthorId('1');
+        $article->setCreatedAt(date('Y-m-d H:i:s'));
+
+        $article->save();
     }
 }
--- src/MyProject/Models/Articles/Article.php
+++ src/MyProject/Models/Articles/Article.php
@@ -7,40 +7,47 @@ use MyProject\Models\Users\User;
 
 class Article extends ActiveRecordEntity
 {
-    /** @var string */
     protected $name;
 
-    /** @var string */
     protected $text;
 
-    /** @var int */
     protected $authorId;
 
-    /** @var string */
     protected $createdAt;
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getText(): string
     {
         return $this->text;
     }
 
-    /**
-     * @return User
-     */
     public function getAuthor(): User
     {
         return User::getById($this->authorId);
+    }
+
+    public function setName(string $name) : void
+    {
+        $this->name = $name;
+    }
+
+    public function setText(string $text) : void
+    {
+        $this->text = $text;
+    }
+
+    public function setAuthorId(string $authorId) : void
+    {
+        $this->authorId = $authorId;
+    }
+
+    public function setCreatedAt(string $createdAt) : void
+    {
+        $this->createdAt = $createdAt;
     }
 
     protected static function getTableName(): string
